3b834e7e73cb9e59589eb41907c71ca8881bf5d3
Ahmed El-Daly <aeldaly@tucows.com>
2024-12-19 21:42:48 +0200

    Fixed most problems. XCode can't find header files

--- apps/monee-mobile/src/app/App.tsx
+++ apps/monee-mobile/src/app/App.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { createStaticNavigation, NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import FaqScreen from './screens/FaqScreen';
+// import { createStackNavigator } from '@react-navigation/stack';
 
-const Stack = createNativeStackNavigator();
-
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        {/* Define your screens here */}
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Faq" component={FaqScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+// const Stack = createNativeStackNavigator();
+//
+// const App = () => {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Home">
+//         {/* Define your screens here */}
+//         <Stack.Screen name="Home" component={HomeScreen} />
+//         <Stack.Screen name="Faq" component={FaqScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
 
 // const RootStack = createNativeStackNavigator({
 //   initialRouteName: 'Home',
@@ -33,4 +34,20 @@ const App = () => {
 //       <Navigation />
 //   );
 // };
-export default App;
+
+const RootNavigation = () => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Faq" component={FaqScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default function App() {
+  return <RootNavigation />;
+}
--- apps/monee-mobile/src/main.tsx
+++ apps/monee-mobile/src/main.tsx
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import { enableScreens } from 'react-native-screens';
 import App from './app/App';
